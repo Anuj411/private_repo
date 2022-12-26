b933e051c18e61f7ffc9c565fc3f2223ab583f28
Anuj <panchalanuj411@gmail.com>
2022-12-26 22:50:29 +0530

    First commit

---
+++ Demo.py
@@ -0,0 +1,9 @@
+print("Hello")
+x=100
+print(11//2)
+
+new_list=["Henry","Tim"]
+sample_list=["Mark",5,"Jack",9, "Chan",5]
+sample_list+=new_list
+sample_list=sample_list+new_list
+print(sample_list)
